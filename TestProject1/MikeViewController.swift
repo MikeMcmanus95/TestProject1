052e9832bdf083bdbf562a03761947c1646a037d
MikeMcmanus95 <mikemcmanus95@gmail.com>
2019-04-10 14:56:38 -0400

    First commit

---
+++ TestProject1/MikeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MikeViewController.swift
+//  TestProject1
+//
+//  Created by Michael Mcmanus on 4/10/19.
+//  Copyright © 2019 Michael Mcmanus. All rights reserved.
+//
+
+import UIKit
+
+class MikeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
